900efc176a2db5b39663c6af16e4b6b4e15e8c69
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-02-26 15:56:23 +0530

    filter meetup finish

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,20 +1,38 @@
 <script>
     import MeetupItem from './MeetupItem.svelte';
+    import MeetupFilter from './MeetupFilter.svelte';
 
     export let meetups;
+    let favOnly = false;
+
+    $: filteredMeetups = favOnly ? meetups.filter(meetup => meetup.isFavorite) : meetups;
+
+    function setFilter(event){
+        if(event.detail === 1){
+            favOnly = true;
+        }else{
+            favOnly = false;
+        }
+    }
+
 </script>
 
-<section id="meetups">
-    {#each meetups as meetup}
-        <MeetupItem id="{meetup.id}" title="{meetup.title}" subtitle="{meetup.subtitle}"
-                    imageUrl="{meetup.imageUrl}" description="{meetup.description}"
-                    address="{meetup.address}" email="{meetup.contactEmail}"
-                    isFav="{meetup.isFavorite}" on:showdetails on:edit/>
-    {/each}
+<section id="meetup-control">
+    <MeetupFilter on:select={setFilter}/>
+
+    <section id="meetups">
+
+        {#each filteredMeetups as meetup}
+            <MeetupItem id="{meetup.id}" title="{meetup.title}" subtitle="{meetup.subtitle}"
+                        imageUrl="{meetup.imageUrl}" description="{meetup.description}"
+                        address="{meetup.address}" email="{meetup.contactEmail}"
+                        isFav="{meetup.isFavorite}" on:showdetails on:edit/>
+        {/each}
+    </section>
 </section>
 
 <style>
-    section {
+    #meetups {
         width: 100%;
         display: grid;
         grid-template-columns: 1fr;
@@ -22,8 +40,12 @@
     }
 
     @media (min-width: 768px) {
-        section {
+        #meetups {
             grid-template-columns: repeat(2, 1fr);
         }
+    }
+
+    #meetup-controls {
+        margin: 1rem;
     }
 </style>
